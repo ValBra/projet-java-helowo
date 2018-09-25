08d3179867161becf40428c437fa71968345871f
Valère Brable <1801042@cgmatane.qc.ca>
2018-09-25 10:32:12 -0400

    Icone appareil photo dans le mur

--- app/src/main/java/ca/qc/cgmatane/informatique/helowo/Mur.java
+++ app/src/main/java/ca/qc/cgmatane/informatique/helowo/Mur.java
@@ -29,6 +29,8 @@ public class Mur extends AppCompatActivity
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                //Envoyer vers l'editeur de publications
             }
         });
 
